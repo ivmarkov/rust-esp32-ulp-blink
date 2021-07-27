4ca268a713fe8f0c8cd6855e21e632546276b390
imarkov <imarkov@vmware.com>
2021-07-27 19:04:19 +0300

    Mostly working now

--- src/main.rs
+++ src/main.rs
@@ -2,47 +2,35 @@
 #![no_main]
 #![feature(core_intrinsics, start)]
 
-use riscv_rt::entry;
-use esp_idf_hal::prelude::*;
 use esp_idf_hal::delay;
-use esp_idf_hal::ulp::sys::cpu::*;
+use esp_idf_hal::prelude::*;
 
 extern crate panic_halt;
 
 #[no_mangle]
-static mut CYCLES: u32 = 10;
+static mut CYCLES: u32 = 2;
 
-#[entry]
-unsafe fn start() -> ! {
-    main()
-}
-
-fn main() -> ! {
+#[no_mangle]
+fn main() {
     let peripherals = Peripherals::take().unwrap();
     let pins = peripherals.pins;
 
     let mut delay = delay::Ulp;
-    let mut led = pins.gpio16.into_output().unwrap();
+    let mut led = pins.gpio4.into_output_od().unwrap();
 
     while get_cycles() > 0 {
         led.set_high().unwrap();
         delay.delay_ms(1000 as u32);
 
         led.set_low().unwrap();
+        delay.delay_ms(1000 as u32);
 
         decr_cycles();
-    }
-
-    unsafe {
-        wakeup_main_processor();
-        shutdown()
     }
 }
 
 fn get_cycles() -> u32 {
-    unsafe {
-        core::ptr::read_volatile(&mut CYCLES)
-    }
+    unsafe { core::ptr::read_volatile(&mut CYCLES) }
 }
 
 fn decr_cycles() {
